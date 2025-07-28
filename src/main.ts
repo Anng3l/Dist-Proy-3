dc14b5f4498a44009238a905342a56b69ffb1170
Angel Maldonado <151583275+Anng3l@users.noreply.github.com>
2025-07-27 22:53:52 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(coockieParser());
   await app.listen(process.env.PORT ?? 3000);
+  console.log(process.env.PORT ?? 3000);
 }
 bootstrap();
