733dcf62e4e4e79a63ce4ae8de1c82af23b09763
Angel Maldonado <151583275+Anng3l@users.noreply.github.com>
2025-07-28 10:43:23 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { json } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(coockieParser());
+  app.enableCors({
+    //CAMBIAR CUANDO YA SE CUENTE CON EL FRONT DESPLEGADO
+    origin: 'http://127.0.0.1:5500', 
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log(process.env.PORT ?? 3000);
 }
